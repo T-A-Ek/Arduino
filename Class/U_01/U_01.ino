55c96a1d002103c2b6fc3f3667fee9a5c1413e95
T-A-Ek <andreas.eklind@teoretiska.se>
2013-01-08 23:55:45 +0100

    adjustments

--- Class/U_01/U_01.ino
+++ Class/U_01/U_01.ino
@@ -1,8 +1,12 @@
-/* 
+﻿/* 
   Vad som behövs för att starta ett projekt
 */
 
-// Det två sätt att skriva kommentarer, denna rad eller de ovan.
+// Man kan skriva kommentarer i koden. Två "slash" innan texten
+// gör att den inte är med i koden, eller om man ska skriva
+// längre stycken så kan man göra som ovan, "slash stjärna"
+// för att starta kommentaren och sedan "stjärna slash" för att
+// avsluta kommentaren.
 // Detta är alltså inte en del av koden!
 
 void setup() {
